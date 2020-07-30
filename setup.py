fe58e19174559460aa4e815dee074697dfff332b
ProfileID <40308458+ProfileID@users.noreply.github.com>
2020-07-31 01:12:37 +0200

    v.0.2.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("./requirements.txt") as f:
 long_description = pypandoc.convert("./README.md", "rst")
 
 setup(name='ansible2puml',
-      version='0.2.7',
+      version='0.2.8',
       description='Create an PlantUML Activity Diagram from Playbooks or Roles trough Python.',
       long_description=long_description,
       author='profileid',
