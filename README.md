a8526a224d988d676b73a4caba977d266d54d03d
ProfileID <40308458+ProfileID@users.noreply.github.com>
2020-07-31 00:56:18 +0200

    0.2.6

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ansible2puml
-![Python package](https://github.com/ProfileID/ansible2puml/workflows/Python%20package/badge.svg)
+
+![Python package](https://github.com/ProfileID/ansible2puml/workflows/Python%20package/badge.svg) 
+![PyPI Package](https://img.shields.io/pypi/v/ansible2puml)
 
 ## About ansible2puml
 Create an PlantUML activity diagram from playbooks (in future also roles) trough python.
@@ -10,7 +12,12 @@ A .puml file with the PlantUML syntax is generated and a link to display the dia
 Python version >3.6
 
 ## Install package
-Local installation trough pip.
+Install via pypi
+```bash
+pip install ansible2puml
+```
+
+Installation via git
 ```bash
 pip install git+https://github.com/ProfileID/ansible2puml
 ```
--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
 from setuptools import setup, find_packages
+import pypandoc
 
-with open("requirements.txt") as f:
+with open("./requirements.txt") as f:
     required = f.read().splitlines()
 
+long_description = pypandoc.convert("./README.md", "rst")
+
 setup(name='ansible2puml',
-      version='0.2.1',
+      version='0.2.6',
       description='Create an PlantUML Activity Diagram from Playbooks or Roles trough Python.',
-      #   url='http://ictshore.com/',
+      long_description=long_description,
       author='profileid',
       author_email='profileid@protonmail.com',
+      url="https://github.com/ProfileID/ansible2puml",
       license='MIT',
       packages=['ansible2puml'],
       install_requires=required,
       entry_points={
           "console_scripts": ["ansible2puml = ansible2puml.cli:main"]
-      }
+      },
+      classifiers=[
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8'
+      ]
       )
