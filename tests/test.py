acf8c88a5667ab8aaddcb8687a41978b65061472
ProfileID <40308458+ProfileID@users.noreply.github.com>
2020-07-30 23:13:37 +0200

    added print

--- tests/test.py
+++ tests/test.py
@@ -2,4 +2,5 @@ import glob, os
 
 for file in os.listdir("tests/"):
     if file.endswith(".yml") and file.startswith("playbook"):
+        print(f"Testing with {file}")
         os.system(f"ansible2puml --source tests/{file} --destination temp.puml")
