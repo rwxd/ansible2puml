33d4e08b34ad748defe6c6d977dbb970e9461280
ProfileID <40308458+ProfileID@users.noreply.github.com>
2020-07-31 01:59:48 +0200

    changed color of diagram and resolved error

--- ansible2puml/functions.py
+++ ansible2puml/functions.py
@@ -56,12 +56,36 @@ def generatePlantUML(taskArray, destination):
     """
 
     activityTemplate = """@startuml
-{% for item in taskArray %}
-{% if item.bold %}
+skinparam defaultTextAlignment center
+skinparam componentStyle uml2
+
+skinparam activity {
+    FontColor          white
+    AttributeFontColor white
+    FontSize           17
+    AttributeFontSize  15
+    AttributeFontname  Droid Sans Mono
+    BackgroundColor    #527BC6
+    BorderColor        black
+    ArrowColor         #222266
+    ArrowFontSize      15
+}
+
+skinparam activityDiamond {
+    FontColor          white
+    AttributeFontColor white
+    FontSize           17
+    BackgroundColor    #527BC6
+    BorderColor        black
+}
+
+{% for item in taskArray -%}
+{% if item.bold -%}
 :**{{ item.taskDescription }}**;
-{% endif %}
+{% else -%}
 :{{ item.taskDescription }};
-{% endfor %}
+{% endif -%}
+{% endfor -%}
 @enduml
 """
 
