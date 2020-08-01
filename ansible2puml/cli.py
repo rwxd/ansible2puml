5a4bf5efb34d4dabe207c4dd56763614507446d3
ProfileID <40308458+ProfileID@users.noreply.github.com>
2020-08-01 13:32:06 +0200

    included when in plantuml

--- ansible2puml/cli.py
+++ ansible2puml/cli.py
@@ -1,10 +1,18 @@
 import click
-from .functions import parseAnsibleFile
+import os
+from .functions import parseAnsibleFile, generatePlantUML
+
 
 @click.command()
 @click.option("--source", help="Source playbook")
 @click.option("--destination", help="destination file e.g. activity.puml")
 def main(source, destination):
-    if source:
-        parseAnsibleFile(source, destination)
-    
+
+    # check if path is file or folder
+    if os.path.isfile(source):
+        parsed = parseAnsibleFile(source)
+
+        generatePlantUML(parsed, destination)
+
+    elif os.path.isdir(source):
+        print("Directory")
--- tests/test.py
+++ tests/test.py
@@ -2,5 +2,5 @@ import glob, os
 
 for file in os.listdir("tests/"):
     if file.endswith(".yml") and file.startswith("playbook"):
-        print(f"Testing with {file}")
-        os.system(f"ansible2puml --source tests/{file} --destination temp.puml")
+        print(f"Testing {file}")
+        os.system(f"ansible2puml --source tests/{file} --destination {file}.puml")
