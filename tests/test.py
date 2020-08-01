a07bd3a8af8d836ef939c2b468da355373f3a03e
ProfileID <40308458+ProfileID@users.noreply.github.com>
2020-08-01 13:41:28 +0200

    removed release in github from pipeline

--- tests/test.py
+++ tests/test.py
@@ -2,5 +2,4 @@ import glob, os
 
 for file in os.listdir("tests/"):
     if file.endswith(".yml") and file.startswith("playbook"):
-        print(f"Testing {file}")
         os.system(f"ansible2puml --source tests/{file} --destination {file}.puml")
