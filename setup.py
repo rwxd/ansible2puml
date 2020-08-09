0c076c7cfacb6e5e14e5a241b7864f3b6e6154e4
ProfileID <40308458+ProfileID@users.noreply.github.com>
2020-08-09 21:36:14 +0200

    changed from pandoc

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup, find_packages
-import pypandoc
+from os import path
 
 with open("./requirements.txt") as f:
     required = f.read().splitlines()
 
-long_description = pypandoc.convert("./README.md", "rst")
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='ansible2puml',
-      version='0.3',
+      version='0.3.1',
       description='Create an PlantUML Activity Diagram from Playbooks or Roles trough Python.',
       long_description=long_description,
+      long_description_content_type='text/markdown',
       author='profileid',
       author_email='profileid@protonmail.com',
       url="https://github.com/ProfileID/ansible2puml",
