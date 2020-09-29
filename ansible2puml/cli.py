fc60832f57bccd99af5cfebe0006a02e6528896f
profileid <40308458+ProfileID@users.noreply.github.com>
2020-09-29 22:36:34 +0200

    shorter options

--- ansible2puml/cli.py
+++ ansible2puml/cli.py
@@ -4,7 +4,7 @@ from .ansible2puml import ansible2puml
 
 
 @click.command()
-@click.option("--source", help="Source playbook")
-@click.option("--destination", help="destination file e.g. activity.puml")
+@click.option("-s", "--source", help="Source playbook")
+@click.option("-d", "--destination", help="destination file e.g. activity.puml")
 def main(source, destination):
     ansible2puml(source=source, destination=destination)
